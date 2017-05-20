bb50cc8eedc8bc0162b47cfe77c31dbf533374e8
McLemore <erdongshengfu@gmail.com>
2017-05-20 16:16:41 +0800

    to#打球助手

--- ai/642455615.js
+++ ai/642455615.js
@@ -1,20 +1,27 @@
 var schedule = require('node-schedule');
 var qq = require('../qq');
 var chat = function(groupname, message) {
-  if (groupname === '机器人大作战') {
+  if (groupname === '球球大作战') {
     switch(message) {
-      case 'hi':
-        qq.send(['group', groupname, 'helloworld']);
+      case '你是谁':
+        qq.send(['group', groupname, '我是微蚁小莉，你们的篮球宝贝']);
+        break;
+      case '你能做什么':
+        qq.send(['group', groupname, '我会在每天17:55提醒大家去打球，更多功能敬请期待']);
+        break;
+      default:
+        qq.send(['group', groupname, '很抱歉，我还不懂你说什么，更多功能敬请期待']);
         break;
     }
   }
 };
 
 schedule.scheduleJob({
-  hour: 16,
-  minute: 9,
+  hour: 17,
+  minute: 55,
 }, function() {
-  qq.send(['group', '机器人大作战', `当前时间：${new Date().toString()}`]);
+  qq.send(['group', '球球大作战', `当前时间：${new Date().toString()}`]);
+  qq.send(['group', '球球大作战', '打球啦！走起～']);
 });
 
 module.exports = chat;
